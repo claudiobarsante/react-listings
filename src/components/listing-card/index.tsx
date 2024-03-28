b044ec6af58ef46445f902355ec76c154fa2468a
Claudio Barsante <clbmribas@gmail.com>
2024-03-28 18:36:25 -0300

    feat: add listings structure - draft <ListingsPage/> and <ListingCard/>

---
+++ src/components/listing-card/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type ListingCardProps = {
+  id: number;
+  thumbnailURL: string;
+  title: string;
+  location: string;
+  bedrooms: number;
+  bathrooms: number;
+  price: number;
+};
+export function ListingCard(props: ListingCardProps) {
+  const { id, thumbnailURL, title, location, bedrooms, bathrooms, price } = props;
+
+  return (
+    <article className="max-w-sm rounded border border-gray-200 shadow-lg">
+      <figure>
+        <img className="w-full" src={thumbnailURL} alt={title} />
+      </figure>
+      <div className="flex flex-col justify-start gap-2">
+        <h2>{title}</h2>
+        <p>{location}</p>
+        <p>
+          {bedrooms}
+          {bathrooms}
+        </p>
+        <p>${price}</p>
+        <button>View Details</button>
+      </div>
+    </article>
+  );
+}
--- src/pages/listings/index.tsx
+++ src/pages/listings/index.tsx
@@ -1,10 +1,24 @@
+import { ListingCard } from 'components/listing-card';
 import { useListings } from 'hooks/context/use-listings';
+
 export function ListingsPage() {
   const { listings } = useListings();
   console.log('--listings--', listings);
   return (
-    <>
-      <h1>Listing page</h1>
-    </>
+    <section className="m-5 grid grid-cols-10 gap-5">
+      {listings.length > 0 &&
+        listings.map((listing) => (
+          <ListingCard
+            key={listing.Id}
+            id={listing.Id}
+            thumbnailURL={listing.ThumbnailURL}
+            title={listing.Title}
+            location={listing.Location}
+            bedrooms={listing.Bathrooms}
+            bathrooms={listing.Bathrooms}
+            price={listing['Sale Price']}
+          />
+        ))}
+    </section>
   );
 }
