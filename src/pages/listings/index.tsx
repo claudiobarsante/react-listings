169504ff3244c4292ad6c4a14705e0cc6b613b5a
Claudio Barsante <clbmribas@gmail.com>
2024-03-29 15:21:33 -0300

    feat: add <Select/> for bedrooms,bathrooms,parking to ListingsPage

--- src/pages/listings/index.tsx
+++ src/pages/listings/index.tsx
@@ -5,18 +5,43 @@ import { Select } from 'components/select';
 
 import { useListings } from 'hooks/context/use-listings';
 
+type Filter = {
+  bedrooms: number | undefined;
+  bathrooms: number | undefined;
+  parking: number | undefined;
+  price: number | undefined;
+};
+
+const initialFiltersValues: Filter = {
+  bedrooms: undefined,
+  bathrooms: undefined,
+  parking: undefined,
+  price: undefined
+};
+
 export function ListingsPage() {
+  const [filters, setFilters] = useState<Filter>(initialFiltersValues);
   const { listings, filtered, searchListings } = useListings();
 
   const handleClick = () => {
-    searchListings({ bedrooms: 4, bathrooms: undefined, parking: undefined, priceRange: undefined });
+    searchListings({
+      bedrooms: filters.bedrooms,
+      bathrooms: filters.bathrooms,
+      parking: filters.parking,
+      priceRange: filters.price
+    });
   };
 
-  const handleSelectChange = (id: string, value: string) => {};
+  const handleSelectChange = (id: string, value: string) => {
+    setFilters((previous) => ({ ...previous, [id]: parseInt(value) }));
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Select id="bedrooms" label="Bedrooms" maxValue={10} handleSelectChange={handleSelectChange} />
+        <Select id="bathrooms" label="Bathrooms" maxValue={10} handleSelectChange={handleSelectChange} />
+        <Select id="parking" label="Parking" maxValue={10} handleSelectChange={handleSelectChange} />
       </div>
       <section className="m-5 grid grid-cols-10 gap-5">
         {filtered.length > 0 &&
