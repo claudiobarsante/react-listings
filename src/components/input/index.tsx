847772c7eeeaf86cf93a2d67df4c9c1b331d8bec
Claudio Barsante <clbmribas@gmail.com>
2024-03-30 00:03:32 -0300

    feat: add <Input/>

---
+++ src/components/input/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type FromInputProps = ComponentProps<'input'>;
+export function FormInput(props: FromInputProps) {
+  return (
+    <input
+      className={twMerge(
+        'w-full rounded-sm border border-zinc-300 p-1 font-medium',
+        'text-md text-zinc-700 placeholder-zinc-500 shadow-sm outline-none',
+        'focus:border-zinc-400 focus:ring-4 focus:ring-zinc-200'
+      )}
+      {...props}
+    />
+  );
+}
--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,6 +1,7 @@
 import { Listing, useListings } from 'hooks/context/use-listings';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { FormInput } from 'components/input/index';
 
 export function ListingDetailsPage() {
   const [listing, setListing] = useState<Listing>();
@@ -13,9 +14,9 @@ export function ListingDetailsPage() {
   }, [getListing, id]);
 
   return (
-    <main className="grid-cols-listingDetails grid">
+    <main className="grid-cols-listingDetails grid max-w-7xl gap-10">
       <section>
-        <header className="flex justify-between">
+        <div className="flex h-12 justify-between">
           <div>
             <h2>{listing?.Title}</h2>
             <h3>{listing?.Location}</h3>
@@ -24,7 +25,7 @@ export function ListingDetailsPage() {
             <h2>{listing?.['Sale Price']}</h2>
             <h3>{listing?.DateListed}</h3>
           </div>
-        </header>
+        </div>
         <figure>
           <img className="w-full" src={listing?.PictureURL} alt={listing?.Title} />
         </figure>
@@ -51,6 +52,17 @@ export function ListingDetailsPage() {
           </div>
         </div>
         <p>{listing?.Description}</p>
+      </section>
+      <section>
+        <div className="flex h-12 items-center justify-end">
+          <button>Save property</button>
+        </div>
+        <form className="flex flex-col items-center justify-center gap-6 rounded-sm border border-zinc-200 bg-zinc-100 p-6">
+          <h3 className="font-semibold text-zinc-700">Contact Agent</h3>
+          <FormInput id="full-name" name="full-name" type="text" placeholder="Full Name *" />
+          <FormInput id="email" name="email" type="email" placeholder="Email *" />
+          <FormInput id="phone-number" name="phone-number" type="text" placeholder="Phone Number *" />
+        </form>
       </section>
     </main>
   );
