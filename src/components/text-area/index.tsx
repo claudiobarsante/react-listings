d480941fab873859ab6f8df29bd3249cd77252a7
Claudio Barsante <clbmribas@gmail.com>
2024-03-30 02:00:17 -0300

    refactor: add forwardRef to <FormTextArea/>  in order to  register the component's ref (react-hook-form)

--- src/components/text-area/index.tsx
+++ src/components/text-area/index.tsx
@@ -1,11 +1,12 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type FormTextAreaProps = ComponentProps<'textarea'>;
-
-export function FormTextArea(props: FormTextAreaProps) {
+// -- had to add forwardRef in order to o register the component's ref (react-hook-form)
+const FormTextAreaComponent: React.ForwardRefRenderFunction<HTMLTextAreaElement, FormTextAreaProps> = (props, ref) => {
   return (
     <textarea
+      ref={ref}
       className={twMerge(
         'min-h-[120px] w-full rounded-sm border border-zinc-300 p-1 font-medium',
         'text-md text-zinc-700 placeholder-zinc-500 shadow-sm outline-none',
@@ -14,4 +15,5 @@ export function FormTextArea(props: FormTextAreaProps) {
       {...props}
     />
   );
-}
+};
+export const FormTextArea = forwardRef(FormTextAreaComponent);
