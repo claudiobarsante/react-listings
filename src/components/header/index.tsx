2ea400d2223ac5ab130ca0472b6a64de1e3266f4
Claudio Barsante <clbmribas@gmail.com>
2024-03-31 01:16:04 -0300

    feat: add <footer/>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,12 @@
 import number8Logo from 'assets/number8.png';
+import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <div className="mt-2 flex size-36 flex-col items-center justify-center rounded-full bg-black">
       <figure>
-        <img src={number8Logo} alt="number8" aria-label="number 8" height={100} width={100} />
+        <Link to="/">
+          <img src={number8Logo} alt="number8" aria-label="number 8" height={100} width={100} />
+        </Link>
       </figure>
     </div>
   );
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -10,6 +10,13 @@ export function AppLayout() {
       <main className="col-start-2">
         <Outlet />
       </main>
+      <footer className="col-start-2 pl-5">
+        <div className="flex items-center justify-center">
+          <span className="text-sm font-medium text-zinc-500">
+            &copy; <time>{new Date().getFullYear()}</time> Number8 - Frontend assessment
+          </span>
+        </div>
+      </footer>
     </div>
   );
 }
