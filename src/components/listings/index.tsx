5a7853fabc129fcb4f82173b310084e63118cb75
Claudio Barsante <clbmribas@gmail.com>
2024-03-31 23:56:55 -0300

    refactor: className <ListingsSearch/>

--- src/components/listings/index.tsx
+++ src/components/listings/index.tsx
@@ -1,10 +1,10 @@
-import { ListingCard } from 'components/listing-card';
+import { ListingCard } from 'components/listing-card/thumbnail';
 import { useListings } from 'hooks/context/use-listings';
 
 export function Listings() {
   const { listings } = useListings();
   return (
-    <section className="mb-4 grid grid-cols-1 justify-items-center gap-y-4 md:grid-cols-3 md:gap-3 lg:grid lg:grid-cols-4 lg:gap-3">
+    <section className="mb-4 grid h-screen grid-cols-1 justify-items-center gap-y-4 md:grid-cols-3 md:gap-3 lg:grid lg:grid-cols-4 lg:gap-3">
       {listings.length > 0 && listings.map((listing) => <ListingCard key={listing.Id} {...listing} />)}
     </section>
   );
