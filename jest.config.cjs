757bc19b7e1f7db73d57668cf73345372cabe5e1
Claudio Barsante <clbmribas@gmail.com>
2024-04-01 21:34:20 -0300

    -- installation of the package 'history' to help testing react router dom and configuration for jest to mock images

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,5 +7,9 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   moduleDirectories: ['node_modules', 'src'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js'
+  }
 };
